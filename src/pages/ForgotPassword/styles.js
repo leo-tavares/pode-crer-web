6e1e1a25123e5076ca2dec7c17feae426ba8faad
leo-tavares <leonardotavaresdev@gmail.com>
2021-05-21 16:26:20 -0300

    chore: add link de ForgotPassword para login #11

--- src/pages/ForgotPassword/styles.js
+++ src/pages/ForgotPassword/styles.js
@@ -1,5 +1,26 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { COLORS } from "../../styles/COLORS";
+import { shade } from "polished";
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   height: 100vh;
+
+  h1 {
+    margin-bottom: 8px;
+  }
+  p {
+    margin-bottom: 8px;
+  }
+`;
+
+export const Link = styled(NavLink)`
+  margin-left: 2px;
+  text-decoration: none;
+  font-weight: bold;
+  color: ${shade(0.4,COLORS.BTN_PRIMARY)};
 `;
