8155d4de53501c1a117049b494c1049ce5442f4d
GabrielNarcizo <eligcan@hotmail.com>
2021-05-22 17:24:50 -0300

    Not found page created

---
+++ src/components/errorPage/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const NotFound = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const ImageNotFound = styled.img`
+    width: 100vh;
+    height: 100vh;
+`
