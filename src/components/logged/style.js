abd44371b52b4e11790294d4199c6c49c5dc629d
GabrielNarcizo <eligcan@hotmail.com>
2021-05-25 15:13:49 -0300

    Sistema implementado para mostrar quando logado e para deslogar #21

---
+++ src/components/logged/style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const LoggedDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const Logout = styled.button`
+    border: none;
+    background-color: #FF5959;
+    color: white;
+    
+    &:hover {
+        background-color: #fff;
+        border: 2px solid #FF5959;
+        color: #FF5959;
+        padding: 0 10px;
+    }
+
+`
