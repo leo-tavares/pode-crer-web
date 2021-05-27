ed314194d0ca51fa301e5ae0ca877f5fffce860f
GabrielNarcizo <eligcan@hotmail.com>
2021-05-26 22:01:57 -0300

    adicionando input para digitar título do sonho

--- src/components/formMessage/index.js
+++ src/components/formMessage/index.js
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react'
 import { useHistory } from 'react-router';
+import { FormMessage } from './style';
 
 const TextArea = () => {
     const history = useHistory();
@@ -9,12 +10,16 @@ const TextArea = () => {
     }, [history])
 
     return (
-        <div>
+        <FormMessage>
+            <form>
+            <label for="titulo">Título do seu sonho:</label>
+            <input type="text" name="titulo" id="titulo"/>
             <label for="mensagem">Descreva seu sonho:</label>
             <textarea cols="70" rows="10" id="mensagem" class="input-padrao" required></textarea>
+            </form>
             <button onClick={voltarInicio}>Agora não!</button>
             <input type="submit" value="Enviar mensagem" className="enviar"></input>
-        </div>
+        </FormMessage>
     )
 }
 
---
+++ src/components/formMessage/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const  FormMessage = styled.div`
+
+    form{
+        display: flex;
+        flex-direction: column;
+    }
+`
