fe04fb9447968179a48c38e1f2b4353fdc06f9ce
leo-tavares <leonardotavaresdev@gmail.com>
2021-05-28 11:04:58 -0300

    style: centralizando msg de erro

--- src/components/errorPage/style.js
+++ src/components/errorPage/style.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const NotFound = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-        h1{
-            color: #01bf71;
-        }
-`
+  height: 80vh;
+
+  h1 {
+    color: #01bf71;
+  }
+`;
