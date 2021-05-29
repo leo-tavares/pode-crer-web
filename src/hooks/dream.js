2cce6143cec47aced416e3df2639b9d40d99cbab
leo-tavares <leonardotavaresdev@gmail.com>
2021-05-28 23:56:42 -0300

    fix: hook de upload de imagem e creação de sonho

--- src/hooks/dream.js
+++ src/hooks/dream.js
@@ -1,43 +1,58 @@
 import { createContext, useCallback, useContext, useState } from "react";
 import api from "../services/api";
+import { useAuth } from "./auth";
 
 const DreamContext = createContext({});
 
 export const DreamProvider = ({ children }) => {
   const [dream, setDream] = useState({});
   const [current, setCurrent] = useState();
+  const { token } = useAuth();
   const getDreams = useCallback(async () => {
     const { data } = await api.get("/dream");
     setDream(data);
   }, []);
 
   const setCurrentDream = useCallback((current) => {
-    setCurrent(current)
-  },[])
-
-  const uploadImage = useCallback(async (id, img) => {
-    const formData = new FormData();
-    formData.append("imagem", img);
-    const { data } = await api.post(`/dream/picture/${id}`, formData, {
-      headers: {
-        "Content-Type": `multipart/form-data;`,
-      },
-    });
+    setCurrent(current);
   }, []);
 
+  const uploadImage = useCallback(
+    async (id, img) => {
+      const formData = new FormData();
+      formData.append("imagem", img);
+      const { data } = await api.post(`/dream/picture/${id}`, formData, {
+        headers: {
+          "Content-Type": `multipart/form-data;`,
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    },
+    [token]
+  );
+
   const createDream = useCallback(async () => {
-    const { data } = await api.post("/dream", {
-      ...dream.newDream,
-      picture: undefined,
-      userId: 1,
-    });
+    console.log(token);
+    const { data } = await api.post(
+      "/dream",
+      {
+        ...dream.newDream,
+        picture: undefined,
+        userId: 1,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     await uploadImage(data.dream.id, dream.newDream?.picture);
-  }, [dream.newDream, uploadImage]);
+  }, [dream.newDream, uploadImage, token]);
 
   const searchSpecifcDreams = useCallback(async (title) => {
-    const {data} = await api.get(`/dream/search/${title}/1`)
+    const { data } = await api.get(`/dream/search/${title}/1`);
     setDream(data);
-  }, [])
+  }, []);
 
   const setCreateData = useCallback(
     async (key, value) => {
@@ -51,7 +66,12 @@ export const DreamProvider = ({ children }) => {
     },
     [dream]
   );
-    console.log(dream);
+
+  // const donate = useCallback(async (payload) => {
+  //   const {} = await api.get()
+  // })
+
+  console.log(dream);
   return (
     <DreamContext.Provider
       value={{
@@ -62,7 +82,7 @@ export const DreamProvider = ({ children }) => {
         createDream,
         setCreateData,
         searchSpecifcDreams,
-        setCurrentDream
+        setCurrentDream,
       }}
     >
       {children}
